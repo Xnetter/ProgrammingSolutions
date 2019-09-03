4891d5044af72583cf73513f9be5b6f76bda3564
Xnetter <ursingarrett@gmail.com>
2019-09-02 17:20:40 -0700

    Merge Sort and Quick Sort added.

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -3,24 +3,11 @@ package com.algorithms;
 import java.util.HashSet;
 import java.util.ArrayList;
 import com.algorithms.stringsandarrays.PrintMatrix;
-import com.algorithms.stringsandarrays.MergeSort;
+import com.algorithms.stringsandarrays.Sort;
 public class Main {
-
-
-
     public static void main(String[] args) {
-        int[] array = {-1,6,3,-10};
-        MergeSort.Sort(array);
+        int[] array = {2,-1,2,2,38};
+        Sort.QuickSort(array);
         PrintMatrix.printArray(array);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        int x = 4;
-        arr.add(x);
-        while(arr.size() < 3){
-            x = 2;
-            arr.add(x);
-        }
-        for(int y : arr){
-            System.out.println(y);
-        }
     }
 }
---
+++ src/com/algorithms/stringsandarrays/Sort.java
@@ -0,0 +1,89 @@
+package com.algorithms.stringsandarrays;
+
+public class Sort {
+    public static void MergeSort(int[] array) {
+        MergeSort(array, new int[array.length], 0, array.length - 1);
+    }
+
+    public static void MergeSort(int[] array, int[] temp, int low, int high) {
+        if (low >= high)
+            return;
+        int middle = (high - low) / 2 + low;
+        MergeSort(array, temp, low, middle);
+        MergeSort(array, temp, middle + 1, high);
+        mergeHalves(array, temp, low, high);
+    }
+
+    public static void mergeHalves(int[] array, int[] temp, int low, int high) {
+        int left = low;
+        int middle = (high - low) / 2 + low;
+        int right = middle + 1;
+        int index = left;
+        while (left <= middle && right <= high) {
+            if (array[left] < array[right]) {
+                temp[index++] = array[left++];
+            } else
+                temp[index++] = array[right++];
+        }
+        System.arraycopy(array, left, temp, index, middle - left + 1);
+        System.arraycopy(array, right, temp, index, high - right + 1);
+        for (int i = low; i <= high; i++) {
+            array[i] = temp[i];
+        }
+
+    }
+
+    public static void QuickSort(int[] array) {
+        QuickSort(array, 0, array.length-1);
+    }
+
+    public static void QuickSort(int[] array, int low, int high) {
+        if(low >= high)
+            return;
+        int middle = (high - low)/2 + low;
+        int pivot = array[middle];
+        int left = low;
+        int right = high;
+        while(left < right) {
+            while(array[left] < pivot){
+                left++;
+            }
+            while(array[right] > pivot){
+                right--;
+            }
+            swap(array, left++, right--);
+        }
+
+        QuickSort(array, low, middle); //Sort left half
+        QuickSort(array, middle+1, high); //Sort right half
+    }
+
+    public static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

33e53bcd6f3b0a019932094a8ae11256f8abc08b
Xnetter <ursingarrett@gmail.com>
2019-10-06 14:21:25 -0700

    Added Memoization and Dynamic Programming to Climb Solution

--- src/com/algorithms/dynamicprogramming/ClimbingStairs.java
+++ src/com/algorithms/dynamicprogramming/ClimbingStairs.java
@@ -2,6 +2,7 @@ package com.algorithms.dynamicprogramming;
 
 public class ClimbingStairs {
     //You can take stairs one or two at a time. How many possible ways can you take n stairs?
+    //Memoization
     int climbingStairs(int n) {
         return climbingStairs(n, new int[n]);
     }
@@ -20,4 +21,18 @@ public class ClimbingStairs {
         return mem[n-1];
     }
 
+    //Dynamic Programming
+    int climbingStairsDP(int n) {
+        int[] dp = new int[n];
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
 }
